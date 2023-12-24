4d9bc31209cb96e430c993195eefe13c130a731b
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2023-12-24 18:18:52 +0200

    Fix Address

--- src/modules/driver/requests/update-driver-location.request.ts
+++ src/modules/driver/requests/update-driver-location.request.ts
@@ -2,10 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateDriverLocationRequest {
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
-  address: string;
 
   @ApiProperty({ type: 'double precision' })
   @IsNotEmpty()
