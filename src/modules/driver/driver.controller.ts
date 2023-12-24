522caa633bcd0b21b4388e0aa6c84af6c15eeeda
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2023-12-24 18:25:48 +0200

    Fix bug 2

--- src/modules/driver/driver.controller.ts
+++ src/modules/driver/driver.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Inject, Param, Put } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Inject, Param, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
 import { DriverService } from './driver.service';
 import { I18nResponse } from 'src/core/helpers/i18n.helper';
 import { Driver } from 'typeorm';
@@ -7,6 +7,17 @@ import { DriverResponse } from './response/driver.response';
 import { plainToClass } from 'class-transformer';
 import { UpdateDriverLocationRequest } from './requests/update-driver-location.request';
 import { ActionResponse } from 'src/core/base/responses/action.response';
+import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
+import { RolesGuard } from '../authentication/guards/roles.guard';
+import { Roles } from '../authentication/guards/roles.decorator';
+import { Role } from 'src/infrastructure/data/enums/role.enum';
+@ApiBearerAuth()
+@ApiHeader({
+  name: 'Accept-Language',
+  required: false,
+  description: 'Language header: en, ar',
+})
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('Driver')
 @Controller('driver')
 export class DriverController {
@@ -31,6 +42,7 @@ export class DriverController {
     0;
     return new ActionResponse(this._i18nResponse.entity(driverResponse));
   }
+  @Roles(Role.DRIVER)
   @Put(':driver_id/location')
   async updateDriverLocation(
     @Body() updateDriverLocationRequest: UpdateDriverLocationRequest,
