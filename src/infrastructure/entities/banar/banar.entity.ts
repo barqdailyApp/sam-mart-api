4000f669b1d2e51d5e1b69571e1b3dbc04fc61fb
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-08 00:51:03 +0200

    Add Banar entity

---
+++ src/infrastructure/entities/banar/banar.entity.ts
@@ -0,0 +1,17 @@
+import { AuditableEntity } from 'src/infrastructure/base/auditable.entity';
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class Banar extends AuditableEntity {
+    @Column()
+    banar: string;
+
+    @Column()
+    started_at: Date;
+
+    @Column()
+    ended_at: Date;
+
+    @Column()
+    is_active: boolean;
+}
