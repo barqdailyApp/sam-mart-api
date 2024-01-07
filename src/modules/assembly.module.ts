1c9cf773a4c51c0219d8967b97851e2fbc0cd817
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-08 00:54:37 +0200

    Add BanarModule and config AssemblyModule to contain BanarModule

--- src/modules/assembly.module.ts
+++ src/modules/assembly.module.ts
@@ -13,6 +13,7 @@ import { SectionModule } from './section/section.module';
 import { CategoryModule } from './category/category.module';
 import { SubcategoryModule } from './subcategory/subcategory.module';
 import { ProductCategoryPriceModule } from './product-category-price/product-category-price.module';
+import { BanarModule } from './banar/banar.module';
 
 @Module({
   imports: [
@@ -27,7 +28,8 @@ import { ProductCategoryPriceModule } from './product-category-price/product-cat
     AdditionalServiceModule,
     SectionModule,
     CategoryModule,
-    ProductCategoryPriceModule
+    ProductCategoryPriceModule,
+    BanarModule,
   ],
   exports: [],
   providers: [],
