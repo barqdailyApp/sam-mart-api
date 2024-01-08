50060d41db482a29bedf1488c971c161327a1c01
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-08 02:00:03 +0200

    Add updateBanar method to BanarService

--- src/modules/banar/banar.service.ts
+++ src/modules/banar/banar.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseService } from 'src/core/base/service/service.base';
 import { LessThan, LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
@@ -6,6 +6,7 @@ import { Banar } from 'src/infrastructure/entities/banar/banar.entity';
 import { CreateBanarRequest } from './dto/request/create-banar.request';
 import { FileService } from '../file/file.service';
 import { UploadFileRequest } from '../file/dto/requests/upload-file.request';
+import { UpdateBannerRequest } from './dto/request/update-banner.request';
 
 @Injectable()
 export class BanarService extends BaseService<Banar> {
@@ -33,13 +34,37 @@ export class BanarService extends BaseService<Banar> {
     }
 
     async getBanars() {
-        return await this.banarRepository.find({ 
-            where: { 
+        return await this.banarRepository.find({
+            where: {
                 is_active: true,
                 started_at: LessThanOrEqual(new Date()),
                 ended_at: MoreThanOrEqual(new Date())
 
             }
         });
+    }
+
+    async updateBanar(id: string, banar: UpdateBannerRequest) {
+        let tempImage = null;
+        const banarEntity = await this.banarRepository.findOne({ where: { id } });
+        if (!banarEntity) {
+            throw new NotFoundException("Banar not found");
+        }
+
+        if (banar.banar) {
+            tempImage = await this._fileService.upload(
+                banar.banar,
+                `banars/`,
+            );
+        }
+
+        Object.assign(banarEntity, { 
+            banar: banar.banar ? tempImage : banarEntity.banar,
+            started_at: banar.started_at ? banar.started_at : banarEntity.started_at,
+            ended_at: banar.ended_at ? banar.ended_at : banarEntity.ended_at,
+            is_active: banar.is_active ? banar.is_active : banarEntity.is_active,
+        });
+
+        return await this.banarRepository.save(banarEntity);
     }
 }
