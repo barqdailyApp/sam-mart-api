bad818ed239ec529148673a53add9548f37c5aec
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-09 23:01:25 +0200

    Add Patch method to update user profile

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,6 +7,7 @@ import {
   Inject,
   Param,
   ParseBoolPipe,
+  Patch,
   Post,
   Put,
   Query,
@@ -60,24 +61,34 @@ export class UserController {
     private readonly _service: UserService,
     @Inject(REQUEST) readonly request: Request,
     @Inject(I18nResponse) private readonly _i18nResponse: I18nResponse,
-  ) {}
+  ) { }
 
   @Put('allow-notification/:allow_notification')
   async allowNotification(
     @Param('allow_notification') allow_notification: boolean,
   ) {
-    return new ActionResponse( await this._service.allowNotification(allow_notification));
+    return new ActionResponse(await this._service.allowNotification(allow_notification));
   }
 
-
-
- @Roles(Role.ADMIN)
+  @Roles(Role.ADMIN)
   @Put('make-resturant')
-async makeResturant(@Body() id:string){
+  async makeResturant(@Body() id: string) {
 
-const user= await this._service.findOne(id)
-user.roles.push(Role.RESTURANT)
-return new ActionResponse( await this._service.update(user))
+    const user = await this._service.findOne(id)
+    user.roles.push(Role.RESTURANT)
+    return new ActionResponse(await this._service.update(user))
 
-}
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor, FileInterceptor('avatarFile'))
+  @ApiConsumes('multipart/form-data')
+  @Patch('update-profile')
+  async updateProfile(
+    @Body() req: UpdateProfileRequest,
+    @UploadedFile(new UploadValidator().build())
+    avatarFile: Express.Multer.File,
+  ) {
+    if(avatarFile) req.avatarFile = avatarFile
+    return new ActionResponse(await this._service.updateProfile(req));
+  }
 }
