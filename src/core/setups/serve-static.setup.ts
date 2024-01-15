eb91bc1d8b65b936209599280af24c62e3b7f5c3
LENOVO <a.maghraby96@yahoo.com>
2024-01-15 14:17:30 +0200

    404 error

--- src/core/setups/serve-static.setup.ts
+++ src/core/setups/serve-static.setup.ts
@@ -1,9 +1,10 @@
 import { join } from "path";
 import { ServeStaticModule } from "@nestjs/serve-static";
 
-export default () => (
-  ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', '..', '..', 'public'),
-    exclude: ['/api*', '/v1*'],
-  })
-);
+// export default () => (
+//   ServeStaticModule.forRoot({
+
+//     rootPath: join(__dirname, '..', '..', '..', 'public'),
+//     exclude: ['/api*', '/v1*'],
+//   })
+// );
