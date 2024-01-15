de7cc1d26c0ebab864bef5538fc69f10db57155a
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-15 16:13:16 +0200

    Enable ServeStaticSetup in index.ts

--- src/core/setups/index.ts
+++ src/core/setups/index.ts
@@ -3,7 +3,7 @@ import CacheSetup from './cache.setup';
 import ConfigurationSetup from './configuration.setup';
 import DatabaseSetup from './database.setup';
 import StorageSetup from './storage.setup';
-// import ServeStaticSetup from './serve-static.setup';
+import ServeStaticSetup from './serve-static.setup';
 import I18nSetup from './i18n.setup';
 import ScheduleSetup from './schedule.setup';
 
@@ -12,7 +12,7 @@ export default () => [
   DatabaseSetup(), // Database Setup
   StorageSetup(), // Storage Setup
   CacheSetup(), // Cache Setup
-  // ServeStaticSetup(), // Serve Static Setup
+  ServeStaticSetup(), // Serve Static Setup
   I18nSetup(), // I18n Setup
   ScheduleSetup() // Schedule Setup
 ];
