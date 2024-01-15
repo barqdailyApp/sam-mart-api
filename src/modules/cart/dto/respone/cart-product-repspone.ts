29d4c56ee29bdd77ce981ed5c583c95413dd2ee8
LENOVO <a.maghraby96@yahoo.com>
2024-01-15 13:48:07 +0200

    cart

--- src/modules/cart/dto/respone/cart-product-repspone.ts
+++ src/modules/cart/dto/respone/cart-product-repspone.ts
@@ -7,19 +7,22 @@ export class CartProductRespone {
   quantity: number;
   total_price: number;
   unit: string;
-  constructor(data: Partial<CartProductRespone>) {
-    this.product = {
+  constructor(data: any) {
+   return {
       id: data.product.id,
 
-      name: data.product.name,
-      section_id: data.section_id,
-
-      description: data.product.description,
+      name: data.product.product_sub_category.product.name,
+      section_id:
+        data.product.product_sub_category.category_subCategory.section_category
+          .section_id,
+      description: data.product.product_sub_category.product.description,
       total_price: data.total_price,
       quantity: data.quantity,
-      unit: data.unit,
+      unit: data.product.product_measurement.measurement_unit.name,
       is_recovered: data.product.is_recovered,
-      image: data.product.product_images.find((e) => e.is_logo),
-    };
+      image: data.product.product_sub_category.product.product_images.find(
+        (e) => e.is_logo,
+      ),
+    } as any;
   }
 }
