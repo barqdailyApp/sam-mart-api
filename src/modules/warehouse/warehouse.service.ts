147ffb45129d319ab78cc0c15a8f846f8469d2a2
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-21 23:25:38 +0200

    Add updateWarehouse method to WarehouseService

--- src/modules/warehouse/warehouse.service.ts
+++ src/modules/warehouse/warehouse.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseService } from 'src/core/base/service/service.base';
 import { BaseUserService } from 'src/core/base/service/user-service.base';
@@ -6,17 +6,34 @@ import { Warehouse } from 'src/infrastructure/entities/warehouse/warehouse.entit
 import { Repository } from 'typeorm';
 import { WarehouseOperationTransaction } from './util/warehouse-opreation.transaction';
 import { WarehouseOperationRequest } from './dto/requests/warehouse-operation.request';
+import { UpdateWarehouseRequest } from './dto/requests/update-warehouse.request';
+import { RegionService } from '../region/region.service';
 
 @Injectable()
 export class WarehouseService extends BaseService<Warehouse> {
 
-constructor(@InjectRepository(Warehouse) private readonly warehouse_repo: Repository<Warehouse>,
-private readonly warehouseOperationTransaction: WarehouseOperationTransaction) {
-    super(warehouse_repo);
-}
+    constructor(
+        @InjectRepository(Warehouse) private readonly warehouse_repo: Repository<Warehouse>,
+        private readonly warehouseOperationTransaction: WarehouseOperationTransaction,
+        @Inject(RegionService) private readonly regionService: RegionService
+        ) {
+        super(warehouse_repo);
+    }
 
-async CreateWAarehouseOperation(request:WarehouseOperationRequest){
-    return  await this.warehouseOperationTransaction.run(request);
-}
+    async CreateWAarehouseOperation(request: WarehouseOperationRequest) {
+        return await this.warehouseOperationTransaction.run(request);
+    }
 
+    async updateWarehouse(id: string, warehouse: UpdateWarehouseRequest) {
+        const warehouseEntity = await this.findOne(id);
+        if (!warehouseEntity) throw new NotFoundException("Warehouse not found");
+
+        if(warehouse.region_id) {
+            const region = await this.regionService.single(warehouse.region_id);
+            if(!region) throw new NotFoundException("Region not found");
+        }
+
+        const updatedWarehouse = Object.assign(warehouseEntity, warehouse);
+        return await this.warehouse_repo.save(updatedWarehouse);
+    }
 }
