8e95bfb75e722dac92ecce46afc8ee6c1b4b2774
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-23 22:21:23 +0200

    Add driver status check in RolesGuard

--- src/modules/authentication/guards/roles.guard.ts
+++ src/modules/authentication/guards/roles.guard.ts
@@ -1,13 +1,20 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from 'src/infrastructure/data/enums/role.enum';
 import { ROLES_KEY } from './roles.decorator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Driver } from 'src/infrastructure/entities/driver/driver.entity';
+import { Repository } from 'typeorm';
+import { DriverStatus } from 'src/infrastructure/data/enums/driver-status.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private reflector: Reflector) { }
+    constructor(
+        private reflector: Reflector,
+        @InjectRepository(Driver) private readonly driverRepository: Repository<Driver>,
+    ) { }
 
-    canActivate(context: ExecutionContext): boolean {
+    async canActivate(context: ExecutionContext) {
         const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
             context.getHandler(),
             context.getClass(),
@@ -19,6 +26,22 @@ export class RolesGuard implements CanActivate {
         const request = context.switchToHttp().getRequest();
         const { user } = request;
 
+        if (requiredRoles.includes(Role.DRIVER)) {
+            await this.checkDriverStatus(user, requiredRoles);
+        }
+
         return requiredRoles.some((role) => user.roles?.includes(role));
+    }
+
+    async checkDriverStatus(user: any, requiredRoles: Role[]) {
+        const driver = await this.driverRepository.findOne({
+            where: { user_id: user.id },
+        });
+
+        if (!driver) throw new UnauthorizedException('invalid_driver');
+
+        if (driver.status !== DriverStatus.VERIFIED) {
+            throw new UnauthorizedException(`You're not verified driver, your account is ${driver.status} now reason: ${driver.status_reason ?? 'no reason specified'}`);
+        }
     }
 }
