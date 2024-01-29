a9dabeafac9f49a5b638e86447b1f4385dbd21d1
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-01-30 00:06:40 +0200

    Add EmployeeResponse class to handle employee data

---
+++ src/modules/employee/dto/response/employee.response.ts
@@ -0,0 +1,18 @@
+import { Expose, Transform, Type, plainToClass } from "class-transformer";
+import { UserResponse } from "src/modules/user/dto/responses/user.response";
+
+export class EmployeeResponse{
+    @Expose() id: string;
+    @Expose() name_ar: string;
+    @Expose() name_en: string;
+    @Expose() qualification: string;
+    @Expose() status: string;
+    @Expose() departements: string[];
+    @Expose() user_id: string;
+    @Expose() country_id: string;
+    @Expose() city_id: string;
+    @Expose() created_at: Date;
+    @Expose() updated_at: Date;
+
+    @Expose() @Type(() => UserResponse) user: UserResponse;
+}
