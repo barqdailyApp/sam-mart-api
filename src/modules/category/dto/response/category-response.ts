ac497ec9e4501d398ebfb9cc84ebd7296a996cea
LENOVO <a.maghraby96@yahoo.com>
2024-01-31 08:48:54 +0200

    name

--- src/modules/category/dto/response/category-response.ts
+++ src/modules/category/dto/response/category-response.ts
@@ -10,6 +10,9 @@ export class CategoryResponse {
     name_ar:string
 
     @Expose()
+    name:string
+
+    @Expose()
     name_en:string
   
     @Expose()
@@ -21,6 +24,7 @@ export class CategoryResponse {
 this.id=data.id
 this.category_id=data.category_id
 this.name_ar=data.name_ar
+this.name=data.name
 this.name_en=data.name_en
 this.logo= toUrl( data.logo)
 this.is_active=data.is_active
