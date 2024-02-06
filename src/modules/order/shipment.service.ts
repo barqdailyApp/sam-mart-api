800dfa225d0b4a0a6075f794032e374d0d9019a9
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-02-06 11:20:29 +0200

    Add ShipmentChat entity and implement initial shipment acceptance message

--- src/modules/order/shipment.service.ts
+++ src/modules/order/shipment.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { BaseUserService } from 'src/core/base/service/user-service.base';
 import { Order } from 'src/infrastructure/entities/order/order.entity';
 import { Request } from 'express';
@@ -11,12 +11,14 @@ import { Shipment } from 'src/infrastructure/entities/order/shipment.entity';
 import { BaseService } from 'src/core/base/service/service.base';
 import { Driver } from 'src/infrastructure/entities/driver/driver.entity';
 import { ShipmentStatusEnum } from 'src/infrastructure/data/enums/shipment_status.enum';
+import { ShipmentChat } from 'src/infrastructure/entities/order/shipment-chat.entity';
 @Injectable()
 export class ShipmentService extends BaseService<Shipment> {
   constructor(
     @InjectRepository(Driver) private driverRepository: Repository<Driver>,
     @InjectRepository(Shipment)
     private shipmentRepository: Repository<Shipment>,
+    @InjectRepository(ShipmentChat) private shipmentChatRepository: Repository<ShipmentChat>,
     @Inject(REQUEST) private readonly request: Request,
   ) {
     super(shipmentRepository);
@@ -31,15 +33,33 @@ export class ShipmentService extends BaseService<Shipment> {
   }
   async acceptShipment(id: string) {
     const driver = await this.getDriver();
+    if (!driver) {
+      throw new NotFoundException('Driver not found');
+    }
+
     const shipment = await this.shipmentRepository.findOne({
       where: {
         id: id,
         warehouse_id: driver.warehouse_id,
       },
     });
+
+    if (!shipment) {
+      throw new NotFoundException('Shipment not found');
+    }
+
     shipment.order_confirmed_at = new Date();
     shipment.status = ShipmentStatusEnum.CONFIRMED;
+    shipment.driver_id = driver.id;
     await this.shipmentRepository.save(shipment);
+
+    const intialShipmentMessage = this.shipmentChatRepository.create({
+      message: 'Shipment has been accepted',
+      user_id: this.request.user.id,
+      shipment_id: shipment.id,
+    });
+    await this.shipmentChatRepository.save(intialShipmentMessage);
+
     return shipment;
   }
 }
