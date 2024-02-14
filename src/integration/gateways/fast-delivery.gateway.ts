3cfd157e9737cfc11ababd485d6cbd0863e57caf
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-02-14 19:28:24 +0200

    Add shipment status change notifications to FastDeliveryGateway

--- src/integration/gateways/fast-delivery.gateway.ts
+++ src/integration/gateways/fast-delivery.gateway.ts
@@ -43,7 +43,46 @@ export class FastDeliveryGateway
                 socket.driver &&
                 socket.driver?.warehouse_id === payload.shipment.warehouse_id
             ) {
-                socket.emit(`driver_fast_delivery`, payload);
+                socket.emit(`fast_delivery`, payload);
+            }
+        });
+    }
+
+    /** 
+        * @description
+        * notify the driver, user and admin.
+        * notify the driver whose not picked the shipment but without send the driver_id whose accept the shipment
+    */
+    async notifyShipmentStatusChange(payload: SendOfferToDriver) {
+        const connectedSockets: any = this.server.sockets;
+        connectedSockets.forEach((socket: any) => {
+            if (
+                (
+                    socket.driver &&
+                    socket.driver?.id === payload.shipment.driver_id
+                ) || (
+                    socket.user &&
+                    (
+                        socket.user?.id === payload.shipment.order.user_id ||
+                        socket.user?.roles.includes('ADMIN')
+                    )
+                )
+            ) {
+                socket.emit(`fast_delivery`, payload);
+            }
+
+            /**
+             * to notify the dirver whose not picked the shipment
+             * you can check if the driver is not the same as the driver who picked the shipment 
+             * shipment.driver_id ? he is the driver who picket the shipment : he isn't
+             */
+            if (
+                socket.driver
+                && socket.driver?.warehouse_id === payload.shipment.warehouse_id
+                && socket.driver?.id !== payload.shipment.driver_id
+            ) {
+                delete payload.shipment.driver_id;
+                socket.emit(`fast_delivery`, payload);
             }
         });
     }
