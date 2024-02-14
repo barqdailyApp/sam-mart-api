4382c2f333947d1a796254db05b3e05c098e1c2a
ahmedeid6842 <ahmedeid6842@gmail.com>
2024-02-14 18:28:35 +0200

    Implement broadcastOfferToDrivers method to fast-delivery gateway

--- src/integration/gateways/fast-delivery.gateway.ts
+++ src/integration/gateways/fast-delivery.gateway.ts
@@ -14,6 +14,9 @@ import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/infrastructure/entities/user/user.entity';
 import { Repository } from 'typeorm';
+import { Driver } from 'src/infrastructure/entities/driver/driver.entity';
+import { ValidDriverMiddleware } from './middlewares/ws-valid-driver';
+import { SendOfferToDriver } from './interfaces/fast-delivery/send-offer-payload.response';
 
 @WebSocketGateway({ namespace: Gateways.FastDelivery.Namespace, cors: { origin: '*' } })
 @UseGuards(WsJwtAuthGuard)
@@ -22,15 +25,28 @@ export class FastDeliveryGateway
     constructor(
         private configService: ConfigService,
         @InjectRepository(User) private userRepository: Repository<User>,
+        @InjectRepository(Driver) private driverRepository: Repository<Driver>,
     ) { }
-    
+
     afterInit(client: Socket) {
         client.use(SocketAuthMiddleware(this.configService, this.userRepository) as any);
+        client.use(ValidDriverMiddleware(this.driverRepository) as any);
     }
 
     @WebSocketServer()
     server: Server;
 
+    async broadcastOfferToDrivers(payload: SendOfferToDriver) {
+        const connectedSockets: any = this.server.sockets;
+        connectedSockets.forEach((socket: any) => {
+            if (
+                socket.driver &&
+                socket.driver?.warehouse_id === payload.shipment.warehouse_id
+            ) {
+                socket.emit(`driver_fast_delivery`, payload);
+            }
+        });
+    }
 
     handleConnection(client: any) {
         console.log('Order connected', client.id);
--- src/integration/gateways/gateways.module.ts
+++ src/integration/gateways/gateways.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrderGateway } from './order.gateway';
 import { SupportTicketGateway } from './support-ticket.gateway';
 import { ShipmentChatGateway } from './shipment-chat-gateway';
-import { FastDeliveryGateway } from './fast-delivery.gatewau';
+import { FastDeliveryGateway } from './fast-delivery.gateway';
 
 @Module({
     imports: [],
