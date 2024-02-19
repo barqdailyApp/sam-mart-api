f40be83d609e216d2e78436415c2379a50f5b865
LENOVO <a.maghraby96@yahoo.com>
2024-02-19 11:56:28 +0200

    cart service

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -102,6 +102,19 @@ export class CartController {
 
   @Put('/update/:cart-product/service')
   async addRemoveService(@Body() req: AddRemoveCartProductServiceRequest) {
-    return new ActionResponse(await this.cartService.addRemoveService(req));
+    const cart_product = await this.cartService.addRemoveService(req);
+    const result = this._i18nResponse.entity(
+      await this.cartService.getSingleCartProduct(cart_product.id),
+    );
+
+    const response = new CartProductRespone({
+      id: result.id,
+      additional_services: result.additions,
+      price: result.price,
+      quantity: result.quantity,
+      product: result.product_category_price,
+    });
+
+    return new ActionResponse(response);
   }
 }
