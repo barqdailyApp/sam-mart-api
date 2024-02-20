cde097ec52abd60f9541582366317edb25634086
LENOVO <a.maghraby96@yahoo.com>
2024-02-20 10:05:08 +0200

    cart response

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -97,7 +97,19 @@ export class CartController {
 
   @Put('/update/:cart-product')
   async updateCartProduct(@Body() req: UpdateCartProductRequest) {
-    return new ActionResponse(await this.cartService.updatecartProduct(req));
+    const cart_product = await this.cartService.updatecartProduct(req);
+    const result = this._i18nResponse.entity(
+      await this.cartService.getSingleCartProduct(cart_product.id),
+    );
+
+    const response = new CartProductRespone({
+      id: result.id,
+      additional_services: result.additions,
+      price: result.price,
+      quantity: result.quantity,
+      product: result.product_category_price,
+    });
+    return new ActionResponse(response);
   }
 
   @Put('/update/:cart-product/service')
