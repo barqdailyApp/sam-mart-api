f49f0fc96004344cc346b02309564b1206f10da9
LENOVO <a.maghraby96@yahoo.com>
2024-02-20 10:21:27 +0200

    cart product

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -196,7 +196,7 @@ export class CartService extends BaseService<CartProduct> {
         cart_product.quantity + product_category_price.min_order_quantity >=
         product_category_price.max_order_quantity
       )
-        cart_product.quantity = product_category_price.max_order_quantity;
+        cart_product.quantity = cart_product.quantity;
       else cart_product.quantity += product_category_price.min_order_quantity;
     } else {
       if (
