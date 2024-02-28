68b8f9ae326a52d409e25db6914ca8222dced114
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2024-02-28 14:01:58 +0200

    improve shipment

--- src/modules/order/dto/response/client-response/shipment-single.response.ts
+++ src/modules/order/dto/response/client-response/shipment-single.response.ts
@@ -7,25 +7,17 @@ import { Shipment } from 'src/infrastructure/entities/order/shipment.entity';
 export class ShipmentSingleResponse {
   @Expose() shipment_id: string;
   @Expose() status: ShipmentStatusEnum;
-  @Expose() order_confirmed_at: Date;
-  @Expose() order_on_processed_at: Date;
-  @Expose() order_shipped_at: Date;
-  @Expose() order_delivered_at: Date;
+
   @Expose() shipment_products: any;
 
   @Expose() order: any;
 
   @Expose() warehouse: any;
 
-  @Expose() driver: any;
 
   constructor(shipments: Shipment) {
     this.shipment_id = shipments.id;
     this.status = shipments.status;
-    this.order_confirmed_at = shipments.order_confirmed_at;
-    this.order_on_processed_at = shipments.order_on_processed_at;
-    this.order_shipped_at = shipments.order_shipped_at;
-    this.order_delivered_at = shipments.order_delivered_at;
 
     this.order = {
       id: shipments.order.id,
@@ -39,7 +31,6 @@ export class ShipmentSingleResponse {
       client: {
         id: shipments.order.user.id,
         name: shipments.order.user.name,
-        email: shipments.order.user.email,
         phone: shipments.order.user.phone,
         avatar: toUrl(shipments.order.user.avatar),
       },
@@ -89,16 +80,10 @@ export class ShipmentSingleResponse {
         name_en: shipments.warehouse.name_en,
         latitude: shipments.warehouse.latitude,
         longitude: shipments.warehouse.longitude,
+        address_ar: shipments.warehouse.address_ar,
+        address_en: shipments.warehouse.address_en,
       });
 
-    this.driver = shipments.driver
-      ? {
-          id: shipments.driver.id,
-          name: shipments.driver.user.name,
-          phone: shipments.driver.user.phone,
-          email: shipments.driver.user.email,
-          avatar: toUrl(shipments.driver.user.avatar),
-        }
-      : null;
+
   }
 }
