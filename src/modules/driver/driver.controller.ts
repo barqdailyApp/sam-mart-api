15fe6554f6cc0a0f7f35d94db575aab01b860d2d
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2024-03-04 10:43:42 +0200

    all driver , single , total

--- src/modules/driver/driver.controller.ts
+++ src/modules/driver/driver.controller.ts
@@ -5,6 +5,7 @@ import {
   Inject,
   Param,
   Put,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
@@ -20,6 +21,8 @@ import { RolesGuard } from '../authentication/guards/roles.guard';
 import { Roles } from '../authentication/guards/roles.decorator';
 import { Role } from 'src/infrastructure/data/enums/role.enum';
 import { UpdateDriverReceiveOrdersRequest } from './requests/update-driver-receive-orders';
+import { DriversDashboardQuery } from './filters/driver-dashboard.query';
+import { DriverDashboardResponse } from './response/driver-dashboard.response';
 @ApiBearerAuth()
 @ApiHeader({
   name: 'Accept-Language',
@@ -42,6 +45,30 @@ export class DriverController {
       plainToClass(DriverResponse, driver),
     );
     return new ActionResponse(this._i18nResponse.entity(driversResponse));
+  }
+
+  @Get('all-drivers-dashboard')
+  async allDriversDashboard(
+    @Query() driversDashboardQuery: DriversDashboardQuery,
+  ) {
+    const drivers = await this.driverService.allDriversDashboard(
+      driversDashboardQuery,
+    );
+    const driversResponse = drivers.map(
+      (driver) => new DriverDashboardResponse(driver),
+    );
+    return new ActionResponse(driversResponse);
+  }
+  @Get('single-driver-dashboard/:driver_id')
+  async singleDriverDashboard(@Param('driver_id') id: string) {
+    const driver = await this.driverService.singleDriverDashboard(id);
+    const driverResponse = new  DriverDashboardResponse(driver);
+    return new ActionResponse(driverResponse);
+  }
+  @Get('total-client-dashboard')
+  async totalClientDashboard() {
+    const total = await this.driverService.totalClientDashboard();
+    return new ActionResponse(total);
   }
 
   @Get(':driver_id/single-driver')
