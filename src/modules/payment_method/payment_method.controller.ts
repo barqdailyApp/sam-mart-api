cf8f4e8c25df6d597dce1a01804cc761b177de05
LENOVO <a.maghraby96@yahoo.com>
2024-03-11 11:12:54 +0200

    wallet payment

--- src/modules/payment_method/payment_method.controller.ts
+++ src/modules/payment_method/payment_method.controller.ts
@@ -1,22 +1,31 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { PaymentMethodService } from './payment_method.service';
 import { PaginatedRequest } from 'src/core/base/requests/paginated.request';
 import { ActionResponse } from 'src/core/base/responses/action.response';
 import { toUrl } from 'src/core/helpers/file.helper';
-import { ApiTags } from '@nestjs/swagger';
-
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
+import { I18nResponse } from 'src/core/helpers/i18n.helper';
+@ApiHeader({
+    name: 'Accept-Language',
+    required: false,
+    description: 'Language header: en, ar',
+  })
 @ApiTags('Payment Method')
 @Controller('payment-method')
 export class PaymentMethodController {
-  constructor(private readonly paymentService: PaymentMethodService) {}
+  constructor(private readonly paymentService: PaymentMethodService,
+    @Inject(I18nResponse) private readonly _i18nResponse: I18nResponse,) {}
 
   @Get()
   async getPaymentMethods(@Query() query: PaginatedRequest) {
     return new ActionResponse(
+        this._i18nResponse.entity(
+            
+        
       (await this.paymentService.findAll(query)).map((payment) => {
         payment.logo = toUrl(payment.logo);
         return payment;
-      }),
+      })),
     );
   }
 }
