ca10fbcb607b01bfd2f38873ced5a92f1666c69f
Ahmed Eid <ahmedeid6842@gmail.com>
2024-03-12 15:49:37 +0200

    Refactor ReasonService to extend BaseService and add createReason method

--- src/modules/reason/reason.service.ts
+++ src/modules/reason/reason.service.ts
@@ -1,10 +1,19 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BaseService } from 'src/core/base/service/service.base';
 import { Reason } from 'src/infrastructure/entities/reason/reason.entity';
 import { Repository } from 'typeorm';
+import { CreateReasonRequest } from './dto/request/create-reason.request';
 @Injectable()
-export class ReasonService {
+export class ReasonService extends BaseService<Reason>{
     constructor(
         @InjectRepository(Reason) private reasonRepository: Repository<Reason>,
-    ) { }
+    ) { 
+        super(reasonRepository)
+    }
+
+    async createReason(req: CreateReasonRequest): Promise<Reason> {
+     const createdReason = await this.reasonRepository.create(req);
+     return await this.reasonRepository.save(createdReason);
+    }
 }
