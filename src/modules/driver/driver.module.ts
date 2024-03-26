bd163ef5b406a8409f4c153d99c78abc46562926
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2024-03-26 02:06:07 +0200

    Make Update Profile Driver

--- src/modules/driver/driver.module.ts
+++ src/modules/driver/driver.module.ts
@@ -3,11 +3,12 @@ import { DriverController } from './driver.controller';
 import { DriverService } from './driver.service';
 import { GatewaysModule } from 'src/integration/gateways/gateways.module';
 import { DeleteDriverAccountTransaction } from './transactions/delete-driver-account.transaction';
+import { UpdateProfileDriverTransaction } from './transactions/update-profile-driver.transaction';
 
 @Module({
   controllers: [DriverController],
-  providers: [DriverService,DeleteDriverAccountTransaction],
+  providers: [DriverService,DeleteDriverAccountTransaction,UpdateProfileDriverTransaction],
   imports: [GatewaysModule],
-  exports: [DeleteDriverAccountTransaction],
+  exports: [DeleteDriverAccountTransaction,UpdateProfileDriverTransaction],
 })
 export class DriverModule {}
