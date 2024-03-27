90dcc38e2f369d2922434528895726665feef695
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2024-03-27 11:48:59 +0200

    fix bugs

--- src/modules/product-category-price/utils/product-price.transaction.ts
+++ src/modules/product-category-price/utils/product-price.transaction.ts
@@ -66,73 +66,85 @@ export class ProductPriceTransaction extends BaseTransaction<
         product_measurement_id,
       } = measurement_detail;
       //*--------------------------------- Make Check For User Data ---------------------------------*/
-    //* Check if product sub category id exist
-    const productSubCategory = await context.findOne( ProductSubCategory, {
-      where: {
-        id: product_sub_category_id,
-      },
-    });
-    if (!productSubCategory) {
-      throw new NotFoundException('message.product_sub_category_id_not_found');
-    }
-    //* Check  product measurement id
-    const productMeasurement = await context.findOne( ProductMeasurement, {
-      where: { id: product_measurement_id },
-    });
-    if (!productMeasurement) {
-      throw new NotFoundException('message.product_measurement_not_found');
-    }
-
-    //*-------------------------- Check if product category price exist , will update it -------------------*/
-    const productCategoryPrice = await context.findOne( ProductCategoryPrice, {
-      where: {
-        product_measurement_id: product_measurement_id,
-        product_sub_category_id: productSubCategory.id,
-      },
-    });
-    if (productCategoryPrice) {
-      productCategoryPrice.price = price;
-      productCategoryPrice.min_order_quantity = min_order_quantity;
-      productCategoryPrice.max_order_quantity = max_order_quantity;
-      const productCategoryPriceSaved = await context.save( productCategoryPrice);
-      //* Update price for offers
-      const offers = await context.find(ProductOffer, {
+      //* Check if product sub category id exist
+      const productSubCategory = await context.findOne(ProductSubCategory, {
         where: {
-          product_category_price_id: productCategoryPriceSaved.id,
+          id: product_sub_category_id,
         },
       });
-      for (const offer of offers) {
-        if (offer.discount_type == DiscountType.VALUE) {
-          offer.price =
-          productCategoryPriceSaved.price - offer.discount_value;
-        } else {
-          const discountedPercentage =
-            (productCategoryPriceSaved.price * offer.discount_value) / 100;
-          offer.price =
-          productCategoryPriceSaved.price - discountedPercentage;
-        }
-        await context.update(
-          ProductOffer,
-          { id: offer.id },
-          { price: offer.price },
+      if (!productSubCategory) {
+        throw new NotFoundException(
+          'message.product_sub_category_id_not_found',
         );
       }
-      return productCategoryPriceSaved;
-    }
-    //* -------------------------- Create product category price if Not Exist --------------------------*/
+      //* Check  product measurement id
+      const productMeasurement = await context.findOne(ProductMeasurement, {
+        where: { id: product_measurement_id },
+      });
+      if (!productMeasurement) {
+        throw new NotFoundException('message.product_measurement_not_found');
+      }
 
-    const createProductCategoryPrice = context.create( ProductCategoryPrice, {
-      product_measurement_id,
-      product_sub_category_id: productSubCategory.id,
-      price,
-      min_order_quantity,
-      max_order_quantity,
-    });
-    const productCategoryPriceSaved = await context.save(
-      createProductCategoryPrice,
-    );
+      //*-------------------------- Check if product category price exist , will update it -------------------*/
+      const productCategoryPrice = await context.findOne(ProductCategoryPrice, {
+        where: {
+          product_measurement_id: product_measurement_id,
+          product_sub_category_id: productSubCategory.id,
+        },
+      });
+      if (productCategoryPrice) {
+        productCategoryPrice.price = price;
+        productCategoryPrice.min_order_quantity = min_order_quantity;
+        productCategoryPrice.max_order_quantity = max_order_quantity;
+        const productCategoryPriceSaved = await context.save(
+          productCategoryPrice,
+        );
+        //* Update price for offers
+        const offers = await context.find(ProductOffer, {
+          where: {
+            product_category_price_id: productCategoryPriceSaved.id,
+          },
+        });
+        for (const offer of offers) {
+          if (offer.discount_type == DiscountType.VALUE) {
+            if (offer.discount_value == productCategoryPriceSaved.price) {
+              throw new BadRequestException(
+                'message.discount_value_must_be_less_than_price',
+              );
+            }
+            offer.price =
+              productCategoryPriceSaved.price - offer.discount_value;
+          } else {
+            if (offer.discount_value >= 100) {
+              throw new BadRequestException(
+                'message.discount_value_must_be_less_than_100',
+              );
+            }
+            const discountedPercentage =
+              (productCategoryPriceSaved.price * offer.discount_value) / 100;
+            offer.price =
+              productCategoryPriceSaved.price - discountedPercentage;
+          }
+          await context.update(
+            ProductOffer,
+            { id: offer.id },
+            { price: offer.price },
+          );
+        }
+        return productCategoryPriceSaved;
+      }
+      //* -------------------------- Create product category price if Not Exist --------------------------*/
 
-     
+      const createProductCategoryPrice = context.create(ProductCategoryPrice, {
+        product_measurement_id,
+        product_sub_category_id: productSubCategory.id,
+        price,
+        min_order_quantity,
+        max_order_quantity,
+      });
+      const productCategoryPriceSaved = await context.save(
+        createProductCategoryPrice,
+      );
 
       return productCategoryPriceSaved;
     } catch (error) {
