2f41e3586c5e04a5d50a27d124e0fcb40ddd9701
Muhammed Helmy Darwish <Medoplay2012@gmail.com>
2024-03-27 11:17:47 +0200

    fix bug update product price

--- src/modules/product-category-price/product-category-price.module.ts
+++ src/modules/product-category-price/product-category-price.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductCategoryPriceController } from './product-category-price.controller';
 import { ProductCategoryPriceService } from './product-category-price.service';
+import { ProductPriceTransaction } from './utils/product-price.transaction';
 
 @Module({
   controllers: [ProductCategoryPriceController],
-  providers: [ProductCategoryPriceService],
+  providers: [ProductCategoryPriceService,ProductPriceTransaction],
 })
 export class ProductCategoryPriceModule {}
