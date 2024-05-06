725ffe7601ce5768a03a875bcce926b80c5beacd
LENOVO <a.maghraby96@yahoo.com>
2024-05-06 09:32:18 +0300

    add id

--- src/modules/promo-code/dto/request/update-promo-code.request.ts
+++ src/modules/promo-code/dto/request/update-promo-code.request.ts
@@ -3,25 +3,30 @@ import { Transform } from "class-transformer";
 import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class UpdatePromoCodeRequest {
+
     @ApiProperty()
+  
+    @IsString()
+    id: string;
+    @ApiProperty({required:false})
     @IsOptional()
     @IsString()
     code: string;
-    @ApiProperty()
+    @ApiProperty({required:false})
     @IsNumber()
     @IsOptional()
     @Transform(({ value }) => Number(value))
     discount: number;
-    @ApiProperty()
+    @ApiProperty({required:false})
     @IsOptional()
     @IsDate()
     @Transform(({ value }) => new Date(value))
     expire_at: Date;
-    @ApiProperty()
+    @ApiProperty({required:false})
     @IsOptional()
     @IsNumber()
     number_of_uses: number;
-    @ApiProperty()
+    @ApiProperty({required:false})
     @IsOptional()
     @Transform(({ value }) => Boolean(value))
     is_active: boolean;
