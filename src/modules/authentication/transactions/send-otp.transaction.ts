da2d99e973d014601917e532c5b7972afa18b0aa
LENOVO <a.maghraby96@yahoo.com>
2024-06-12 11:10:04 +0300

    sms code

--- src/modules/authentication/transactions/send-otp.transaction.ts
+++ src/modules/authentication/transactions/send-otp.transaction.ts
@@ -41,9 +41,9 @@ export class SendOtpTransaction extends BaseTransaction<
       let code = '1234' 
    
 
-      // if(appEnv=="production"||req.username!="+967777777777"){
-      //      code= randNum(4);
-      //   await this.smsProviderService.sendSms(req.username, `للدخول إلى حسابك في برق  يرجى استخدام الرمز ${code}`);}
+      if(appEnv=="production"||req.username!="+967777777777"){
+           code= randNum(4);
+        await this.smsProviderService.sendSms(req.username, `للدخول إلى حسابك في برق  يرجى استخدام الرمز ${code}`);}
       // map to otp entity
       const otp = plainToInstance(Otp, { ...req, code });
    
