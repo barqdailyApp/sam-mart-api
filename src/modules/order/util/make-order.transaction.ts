75e89bf09a1eed3a8a2c9a574bd7a848e47feb32
LENOVO <a.maghraby96@yahoo.com>
2024-06-15 21:51:16 +0300

    no pics

--- src/modules/order/util/make-order.transaction.ts
+++ src/modules/order/util/make-order.transaction.ts
@@ -242,7 +242,7 @@ export class MakeOrderTransaction extends BaseTransaction<
           order.promo_code_id = promo_code.id;
           order.promo_code_discount = promo_code.discount;
           promo_code.current_uses++;
-          if(promo_code.current_uses==null)
+          if(promo_code.user_ids==null)
             promo_code.user_ids=[]
           promo_code.user_ids.push(user.id);
           await context.save(promo_code);
