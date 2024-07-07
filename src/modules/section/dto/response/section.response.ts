77bd282c8c91de76f539110693fe145b38a27552
LENOVO <a.maghraby96@yahoo.com>
2024-07-07 09:31:24 +0300

    fix

--- src/modules/section/dto/response/section.response.ts
+++ src/modules/section/dto/response/section.response.ts
@@ -12,6 +12,7 @@ export class SectionResponse {
   @Expose() readonly id: string;
 
   @Expose() readonly name_ar: string;
+  @Expose() readonly name: string;
 
   @Expose() readonly name_en: string;
 
