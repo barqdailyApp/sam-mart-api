bcf98e6851d86226565225052fe9ede3b852ac20
LENOVO <a.maghraby96@yahoo.com>
2024-07-13 09:56:46 +0300

    fix

--- src/modules/transaction/dto/requests/make-transaction-request.ts
+++ src/modules/transaction/dto/requests/make-transaction-request.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
 import { TransactionTypes } from "src/infrastructure/data/enums/transaction-types";
 
 export class MakeTransactionRequest {
   @ApiProperty()
   @IsNumber()
+  @Transform((value) => Number(value))
   amount: number;
   @ApiProperty({required:false,enum:TransactionTypes,default:TransactionTypes.ADMIN_RESET})
   @IsEnum(TransactionTypes)
