448112c5b5e80f2df7334f1ad02b79aee20f26dd
LENOVO <a.maghraby96@yahoo.com>
2024-10-06 09:38:07 +0300

    warehous

--- src/modules/product/product-dashboard.service.ts
+++ src/modules/product/product-dashboard.service.ts
@@ -1513,7 +1513,7 @@ export class ProductDashboardService {
       .select('shipment_product.product_id', 'productId')
      
       .leftJoinAndSelect('shipment_product.product', 'product')
-      .leftJoinAndSelect('product.warehouse_operation', 'warehouse_operation')
+      .leftJoinAndSelect('product.warehouse_operations', 'warehouse_operation')
       .where('warehouse_operation.warehouse_id = :warehouse_id', {
         warehouse_id:warehouse_id
       })
