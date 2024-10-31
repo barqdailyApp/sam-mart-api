7c4962367b5aedf94e3a93172273f41c6265947a
LENOVO <a.maghraby96@yahoo.com>
2024-10-31 09:59:45 +0300

    product

--- src/modules/product/dto/response/response-dashboard/single-product-dashboard-new.response.ts
+++ src/modules/product/dto/response/response-dashboard/single-product-dashboard-new.response.ts
@@ -39,7 +39,7 @@ export class SingleProductDashboardNewResponse {
       product_barcode: product.barcode,
       row_number: product.row_number,
       product_keywords: product.keywords,
-      Subcategory: product.product_sub_categories[0].category_subCategory.subcategory,
+      subcategory: product.product_sub_categories[0].category_subCategory.subcategory,
       Warehouse_products: product.warehouses_products,
       quantity_available: product.warehouses_products.reduce(
         (acc, cur) => acc + cur.quantity,
