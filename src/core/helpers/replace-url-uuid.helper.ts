f2aa10fb3b034c42fcdd6c433b3b522b0ce78e59
LENOVO <a.maghraby96@yahoo.com>
2024-12-08 10:27:15 +0200

    employe

--- src/core/helpers/replace-url-uuid.helper.ts
+++ src/core/helpers/replace-url-uuid.helper.ts
@@ -1,5 +1,8 @@
 export function replaceUUIDInURL(url: string): string {
-    const parts = url.split('/');
+    // Split the URL into the path and query parts
+    const [path] = url.split('?'); // Ignore query parameters
+    
+    const parts = path.split('/');
     const uuidRegex = /^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$/i;
     const uuidPlaceholder = '([0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12})';
 
