67cb4d0dbc4cda7c46ffdc95a051f24bff2d12c8
Ahmed <a.maghraby96@yahoo.com>
2025-02-10 11:03:57 +0200

    order socket

--- src/modules/restaurant-order/util/make-restaureant-order.transaction.ts
+++ src/modules/restaurant-order/util/make-restaureant-order.transaction.ts
@@ -102,8 +102,8 @@ order.total_price=total;
                     quantity: cart_meal.quantity,
                     restaurant_order_id:order.id,
                     price: cart_meal.meal.price,
-                    total_price: Number(cart_meal.meal.price)+Number(cart_meal.cart_meal_options.map(cart_meal_option=>cart_meal_option.option.price).reduce((a,b)=>a+b,0)),
-                    restaurant_order_meal_options:cart_meal?.cart_meal_options?.map(cart_meal_option=>{return {option_id:cart_meal_option.option_id,price:cart_meal_option.option.price}})
+                    total_price: Number(cart_meal.meal.price)+Number(cart_meal?.cart_meal_options?.map(cart_meal_option=>cart_meal_option?.option?.price).reduce((a,b)=>a+b,0)),
+                    restaurant_order_meal_options:cart_meal?.cart_meal_options?.map(cart_meal_option=>{return {option_id:cart_meal_option?.option_id,price:cart_meal_option?.option?.price}})
                 }
             )  
             })
