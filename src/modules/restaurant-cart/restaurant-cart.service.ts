801e8f6a3f579e59bfb1e30033d0339a4373e735
Ahmed <a.maghraby96@yahoo.com>
2025-03-09 11:54:36 +0200

    promocode

--- src/modules/restaurant-cart/restaurant-cart.service.ts
+++ src/modules/restaurant-cart/restaurant-cart.service.ts
@@ -30,9 +30,10 @@ export class RestaurantCartService {
     private readonly restaurantCartMealOptionRepository: Repository<RestaurantCartMealOption>,
     @Inject(REQUEST) private readonly request: Request,
     private readonly updateMealRestaurantCartTransaction: UpdateMealRestaurantCartTransaction,
-    @InjectRepository(Address) 
+    @InjectRepository(Address)
     private readonly addressRepository: Repository<Address>,
-    @InjectRepository(Constant) private readonly constantRepository: Repository<Constant>,
+    @InjectRepository(Constant)
+    private readonly constantRepository: Repository<Constant>,
   ) {}
 
   async addMealToCart(req: AddMealRestaurantCartRequest) {
@@ -41,67 +42,156 @@ export class RestaurantCartService {
   async getCartMeals() {
     const cart = await this.restaurantCartRepository.findOne({
       where: { user_id: this.request.user.id },
-      relations: { restaurant_cart_meals:{meal:true,cart_meal_options:{option:true}} , restaurant: true },
+      relations: {
+        restaurant_cart_meals: {
+          meal: true,
+          cart_meal_options: { option: true },
+        },
+        restaurant: true,
+      },
     });
-    const default_address=await this.addressRepository.findOne({where:{user_id:this.request.user.id,is_favorite:true}});
-    const settings=await this.constantRepository.find();
-    const fixed_delivery_fee=settings.find((s)=>s.type===ConstantType.FIXED_DELIVERY_FEE).variable;
-    const delivery_price_per_km=settings.find((s)=>s.type===ConstantType.DELIVERY_PRICE_PER_KM).variable;
-    const fixed_delivery_distance=settings.find((s)=>s.type===ConstantType.FREE_DELIVERY_DISTANCE).variable;
-    const distance=calculateDistances([cart.restaurant.latitude,cart.restaurant.longitude],[default_address.latitude,default_address.longitude]);
+    const default_address = await this.addressRepository.findOne({
+      where: { user_id: this.request.user.id, is_favorite: true },
+    });
+    const settings = await this.constantRepository.find();
+    const fixed_delivery_fee = settings.find(
+      (s) => s.type === ConstantType.FIXED_DELIVERY_FEE,
+    ).variable;
+    const delivery_price_per_km = settings.find(
+      (s) => s.type === ConstantType.DELIVERY_PRICE_PER_KM,
+    ).variable;
+    const fixed_delivery_distance = settings.find(
+      (s) => s.type === ConstantType.FREE_DELIVERY_DISTANCE,
+    ).variable;
+    const distance = calculateDistances(
+      [cart.restaurant.latitude, cart.restaurant.longitude],
+      [default_address.latitude, default_address.longitude],
+    );
     const delivery_fee =
-    Number(fixed_delivery_fee) +
-    (Number(distance) - Number(fixed_delivery_distance) < 0
-      ? 0
-      : Number(distance) - Number(fixed_delivery_distance)) *
-      Number(delivery_price_per_km);
-    
+      Number(fixed_delivery_fee) +
+      (Number(distance) - Number(fixed_delivery_distance) < 0
+        ? 0
+        : Number(distance) - Number(fixed_delivery_distance)) *
+        Number(delivery_price_per_km);
+
     if (!cart) return null;
-    const response = plainToInstance(GetCartMealsResponse, cart.restaurant_cart_meals.map((m) => {const total_unit_price=Number(m.meal.price)+Number(m.cart_meal_options.reduce((acc,curr)=>acc+curr.option.price,0));return {...m.meal,meal_id:m.meal.id,id:m.id,quantity:m.quantity,total_price:total_unit_price}}), {
-      excludeExtraneousValues: true,
-    }); 
-    const restaurant_respone= plainToInstance(RestaurantResponse, cart.restaurant,{excludeExtraneousValues:true});
-    return {  meals: response,restaurant:restaurant_respone,delivery_fee:delivery_fee};
+    const response = plainToInstance(
+      GetCartMealsResponse,
+      cart.restaurant_cart_meals.map((m) => {
+        const total_unit_price =
+          Number(m.meal.price) +
+          Number(
+            m.cart_meal_options.reduce(
+              (acc, curr) => acc + curr.option.price,
+              0,
+            ),
+          );
+        return {
+          ...m.meal,
+          meal_id: m.meal.id,
+          id: m.id,
+          quantity: m.quantity,
+          total_price: total_unit_price,
+          cart_meal_options:m.cart_meal_options
+          
+        };
+      }),
+      {
+        excludeExtraneousValues: true,
+      },
+    );
+    const restaurant_respone = plainToInstance(
+      RestaurantResponse,
+      cart.restaurant,
+      { excludeExtraneousValues: true },
+    );
+    return {
+      meals: response,
+      restaurant: restaurant_respone,
+      delivery_fee: delivery_fee,
+    };
   }
   async clearCart() {
-    const cart= await this.restaurantCartRepository.findOne({
-     where:{ user_id: this.request.user.id},
+    const cart = await this.restaurantCartRepository.findOne({
+      where: { user_id: this.request.user.id },
     });
-    const cart_items= await this.restaurantCartMealRepository.find({
-      where:{cart_id:cart.id},
+    const cart_items = await this.restaurantCartMealRepository.find({
+      where: { cart_id: cart.id },
     });
- 
- return    await this.restaurantCartMealRepository.remove(cart_items);
+
+    return await this.restaurantCartMealRepository.remove(cart_items);
   }
 
   async deleteCartMeal(cart_meal_id: string) {
     return await this.restaurantCartMealRepository.delete({
       id: cart_meal_id,
     });
-  } 
-    async updateCartMeal(req: UpdateCartMealRequest) {
-      return await this.updateMealRestaurantCartTransaction.run(req);
-    }
+  }
+  async updateCartMeal(req: UpdateCartMealRequest) {
+    return await this.updateMealRestaurantCartTransaction.run(req);
+  }
 
-  async getCartMealDetails(cart_meal_id:string){
-    const cart_meal= await this.restaurantCartMealRepository.findOne({where:{id:cart_meal_id},relations:{meal:{meal_option_groups:{option_group:{options:true}}},cart_meal_options:{option:true}}});
-   const response = plainToInstance(GetCartMealsResponse, {...cart_meal.meal,meal_id:cart_meal.meal.id,id:cart_meal.id,quantity:cart_meal.quantity,total_price:Number(cart_meal.meal.price)+ Number(cart_meal.cart_meal_options.reduce((acc,curr)=>acc+curr.option.price,0))}, { excludeExtraneousValues: true });
+  async getCartMealDetails(cart_meal_id: string) {
+    const cart_meal = await this.restaurantCartMealRepository.findOne({
+      where: { id: cart_meal_id },
+      relations: {
+        meal: { meal_option_groups: { option_group: { options: true } } },
+        cart_meal_options: { option: true },
+      },
+    });
+    const response = plainToInstance(
+      GetCartMealsResponse,
+      {
+        ...cart_meal.meal,
+        meal_id: cart_meal.meal.id,
+        id: cart_meal.id,
+        quantity: cart_meal.quantity,
+        total_price:
+          Number(cart_meal.meal.price) +
+          Number(
+            cart_meal.cart_meal_options.reduce(
+              (acc, curr) => acc + curr.option.price,
+              0,
+            ),
+          ),
+      },
+      { excludeExtraneousValues: true },
+    );
     //check if option is selected
     response.option_groups.forEach((option_group) => {
       option_group.options.forEach((option) => {
-        option.is_selected=cart_meal.cart_meal_options.some(cart_meal_option=>cart_meal_option.option.id===option.id)
+        option.is_selected = cart_meal.cart_meal_options.some(
+          (cart_meal_option) => cart_meal_option.option.id === option.id,
+        );
       });
-    })
-    return response
+    });
+    return response;
   }
 
   async getCartTotal() {
-    const cart= await this.restaurantCartRepository.findOne({
-      where:{ user_id: this.request.user.id},relations:{restaurant_cart_meals:{meal:true,cart_meal_options:{option:true}}}
+    const cart = await this.restaurantCartRepository.findOne({
+      where: { user_id: this.request.user.id },
+      relations: {
+        restaurant_cart_meals: {
+          meal: true,
+          cart_meal_options: { option: true },
+        },
+      },
     });
 
-    const data= {meals_count:cart.restaurant_cart_meals.length,total_price:cart.restaurant_cart_meals.reduce((acc,curr)=>acc+(curr.quantity* curr.meal.price+curr.cart_meal_options.reduce((acc,curr)=>acc+curr.option.price,0)),0)};
+    const data = {
+      meals_count: cart.restaurant_cart_meals.length,
+      total_price: cart.restaurant_cart_meals.reduce(
+        (acc, curr) =>
+          acc +
+          (curr.quantity * curr.meal.price +
+            curr.cart_meal_options.reduce(
+              (acc, curr) => acc + curr.option.price,
+              0,
+            )),
+        0,
+      ),
+    };
     return data;
   }
-  
 }
