cf6838c8664eb9fccfd60b3b354c03c6576c1c99
Ahmed <a.maghraby96@yahoo.com>
2025-04-07 08:48:09 +0200

    favorite meals

--- src/modules/banar/dto/request/create-banar.request.ts
+++ src/modules/banar/dto/request/create-banar.request.ts
@@ -31,6 +31,17 @@ export class CreateBanarRequest {
     @IsBoolean()
     is_popup: boolean;
 
+    @ApiProperty({ nullable: true, required: false, default: false })
+    @IsOptional()
+    @Transform(({ value }) => {
+        if (typeof value === 'string') {
+            return value.toLowerCase() === 'true';
+        }
+        return Boolean(value);
+    })
+    @IsBoolean()
+    is_general: boolean;
+
 
 
 }
