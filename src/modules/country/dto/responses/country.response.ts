61efdb243c166a236882b6f0d8b09d3914e0dd3e
Ahmed <a.maghraby96@yahoo.com>
2025-04-22 13:00:54 +0200

    transaltion

--- src/modules/country/dto/responses/country.response.ts
+++ src/modules/country/dto/responses/country.response.ts
@@ -3,6 +3,7 @@ import { Exclude, Expose } from 'class-transformer';
 @Exclude()
 export class CountryResponse {
   @Expose() readonly id: string;
+  @Expose() readonly name: string; 
   @Expose() readonly name_ar: string;
   @Expose() readonly name_en: string;
 
